13f816b671ade64d45e95acc38dfae647a203df2
Chris Zetter <zetter@users.noreply.github.com>
2022-12-10 16:58:36 +0000

    Part 2

--- day_1/Main.hs
+++ day_1/Main.hs
@@ -1,6 +1,12 @@
+import Data.List (sort)
 import Data.List.Split (splitOn)
+
+part1 = do
+  contents <- readFile "data.txt"
+  let groups = map (map read . lines) (splitOn "\n\n" contents)
+  print (maximum (map sum groups))
 
 main = do
   contents <- readFile "data.txt"
   let groups = map (map read . lines) (splitOn "\n\n" contents)
-  print (maximum (map sum groups))
+  print (sum (take 3 (reverse (sort (map sum groups)))))
