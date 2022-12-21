e4073f6f7f1b83571268a9940df229c77ef7226e
Chris Zetter <zetter@users.noreply.github.com>
2022-12-21 15:57:59 +0000

    Day 8 part 2

--- day_8/Main.hs
+++ day_8/Main.hs
@@ -8,6 +8,9 @@ rotateN n s = last (take (n + 1) (iterate rotateOne s))
 orMatrix :: [[Bool]] -> [[Bool]] -> [[Bool]]
 orMatrix = zipWith (zipWith (||))
 
+matrixProduct :: [[Int]] -> [[Int]] -> [[Int]]
+matrixProduct = zipWith (zipWith (*))
+
 rowVisibility :: Int -> String -> [Bool]
 rowVisibility high [] = []
 rowVisibility high (a : rs)
@@ -19,6 +22,19 @@ rowVisibility high (a : rs)
 rowsVisibility :: [String] -> [[Bool]]
 rowsVisibility = map (rowVisibility (-1))
 
+cellScore :: Char -> String -> Int
+cellScore _ [] = 0
+cellScore c (x:xs)
+  | c > x     = 1 + cellScore c xs
+  | otherwise = 1
+
+rowScore :: String -> [Int]
+rowScore [] = []
+rowScore (x : xs) = cellScore x xs : rowScore xs
+
+rowsScore :: [String] -> [[Int]]
+rowsScore = map rowScore
+
 main :: IO ()
 main = do
   contents <- readFile "data.txt"
@@ -29,3 +45,11 @@ main = do
   let bottom = (rotateN 1 . rowsVisibility . rotateN 3) matrix
   let result = foldl1 orMatrix [left, top, right, bottom]
   print $ sum (map (length . filter id) result)
+
+  let left' = rowsScore matrix
+  let top' = (rotateN 3 . rowsScore . rotateN 1) matrix
+  let right' = (rotateN 2 . rowsScore . rotateN 2) matrix
+  let bottom' = (rotateN 1 . rowsScore . rotateN 3) matrix
+  let result' = foldl1 matrixProduct [left', top', right', bottom']
+  print $ maximum (map maximum result')
+
