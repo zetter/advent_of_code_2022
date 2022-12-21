11c169c57728d9fbfa084bb4c56f1400ab49be94
Chris Zetter <zetter@users.noreply.github.com>
2022-12-21 15:27:15 +0000

    refactor

--- day_8/Main.hs
+++ day_8/Main.hs
@@ -5,27 +5,27 @@ rotateN n s = last (take (n + 1) (iterate rotateOne s))
   where
     rotateOne s = map reverse (transpose s)
 
-orRows :: [[[Bool]]] -> [[Bool]]
-orRows x = map orRow (transpose x)
-  where orRow y = map or (transpose y)
+orMatrix :: [[Bool]] -> [[Bool]] -> [[Bool]]
+orMatrix = zipWith (zipWith (||))
 
-mapRow :: Int -> String -> [Bool]
-mapRow high [] = []
-mapRow high (a:rs)
- | parsedA > high = True:mapRow parsedA rs
- | otherwise = False:mapRow high rs
- where parsedA = read [a] :: Int
+rowVisibility :: Int -> String -> [Bool]
+rowVisibility high [] = []
+rowVisibility high (a : rs)
+  | parsedA > high = True : rowVisibility parsedA rs
+  | otherwise = False : rowVisibility high rs
+  where
+    parsedA = read [a] :: Int
 
-mapRows :: [String] -> [[Bool]]
-mapRows = map (mapRow (-1))
+rowsVisibility :: [String] -> [[Bool]]
+rowsVisibility = map (rowVisibility (-1))
 
 main :: IO ()
 main = do
   contents <- readFile "data.txt"
   let matrix = lines contents
-  let left = mapRows matrix
-  let top = (rotateN 3 . mapRows . rotateN 1) matrix
-  let right = (rotateN 2 . mapRows . rotateN 2) matrix
-  let bottom = (rotateN 1 . mapRows . rotateN 3) matrix
-  let result = orRows [left, top, right, bottom]
+  let left = rowsVisibility matrix
+  let top = (rotateN 3 . rowsVisibility . rotateN 1) matrix
+  let right = (rotateN 2 . rowsVisibility . rotateN 2) matrix
+  let bottom = (rotateN 1 . rowsVisibility . rotateN 3) matrix
+  let result = foldl1 orMatrix [left, top, right, bottom]
   print $ sum (map (length . filter id) result)
